46b877735e623d43faf0bb406d4bc6b26277e756
takashi247 <takashi247@gmail.com>
2022-01-28 23:30:11 +0900

    add outputs for constructor and destructor of Dog and Cat class

--- ex00/srcs/Cat.cpp
+++ ex00/srcs/Cat.cpp
@@ -6,11 +6,13 @@ const std::string	Cat::TYPE_NAME = "Cat";
 Cat::Cat():
 	Animal()
 {
+    std::cout << "The constructor of Cat class was called" << std::endl;
 	this->setType(Cat::TYPE_NAME);
 }
 
 Cat::Cat(const Cat &other)
 {
+    std::cout << "The copy constructor of Cat class was called" << std::endl;
 	*this = other;
 }
 
@@ -23,7 +25,9 @@ Cat
 }
 
 Cat::~Cat()
-{}
+{
+    std::cout << "the destructor of Cat class was called" << std::endl;
+}
 
 void
 	Cat::makeSound() const
