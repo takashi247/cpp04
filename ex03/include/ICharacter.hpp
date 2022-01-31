e9e6f7670e61ee24db9d0b3acf00aca98e197482
takashi247 <takashi247@gmail.com>
2022-01-31 13:59:10 +0900

    add new line

--- ex03/include/ICharacter.hpp
+++ ex03/include/ICharacter.hpp
@@ -14,6 +14,7 @@ public:
   virtual void equip(AMateria* m) = 0;
   virtual void unequip(int idx) = 0;
   virtual void use(int idx, ICharacter& target) = 0;
+
 protected:
   ICharacter();
   ICharacter(ICharacter const &other);
