ea5ed0b5364bbfe21aabe5a8eb512dcfd07346e3
takashi247 <takashi247@gmail.com>
2022-01-31 13:57:57 +0900

    make constructors of character interface protected

--- ex03/include/ICharacter.hpp
+++ ex03/include/ICharacter.hpp
@@ -8,14 +8,15 @@ class AMateria;
 class ICharacter
 {
 public:
-  ICharacter();
-  ICharacter(ICharacter const &other);
   ICharacter &operator=(ICharacter const &other);
   virtual ~ICharacter();
   virtual const std::string &getName() const = 0;
   virtual void equip(AMateria* m) = 0;
   virtual void unequip(int idx) = 0;
   virtual void use(int idx, ICharacter& target) = 0;
+protected:
+  ICharacter();
+  ICharacter(ICharacter const &other);
 };
 
 #endif /* ICHARACTER_HPP */
